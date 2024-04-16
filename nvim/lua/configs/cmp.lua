23ee732df4e47274def990e981b405da39860ed5
Adam George <adam@adam-george.com>
2024-04-16 00:40:44 -0400

    nvim config autoformatting
    Just ran all the nvim lua config files through the autoformatter.

--- nvim/lua/configs/cmp.lua
+++ nvim/lua/configs/cmp.lua
@@ -1,31 +1,30 @@
 local status, cmp = pcall(require, "cmp")
 if not status then
-    return
+  return
 end
 
 local lspkind = require("lspkind")
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ["<CR>"] = cmp.config.disable,
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<C-y>"] = cmp.mapping.confirm(),
-    }),
-    sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "buffer" },
-    }),
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<CR>"] = cmp.config.disable,
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<C-y>"] = cmp.mapping.confirm(),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+  }),
 })
 
 vim.cmd([[
   set completeopt=menuone,noinsert,noselect
   highlight! default link CmpItemKind CmpItemMenuDefault
 ]])
-
