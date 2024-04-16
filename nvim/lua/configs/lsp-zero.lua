ef420e7401c9f5680c3ba1fef26be93b0ec3e454
Adam George <adam@adam-george.com>
2024-04-16 00:35:25 -0400

    Fix lsp-zero config
    A couple of language servers need to be manually installed in order to
    use the correct version:
    - ruby_lsp
    - gopls
    - rust_analyzer
    
    This changes the lsp-config to not auto-install and initialize them.
    
    Additionally, I'm using the dev version of nvim (0.10.x), so the extra
    config for ruby is no longer necessary, and was actually breaking
    semantic highlighting.

--- nvim/lua/configs/lsp-zero.lua
+++ nvim/lua/configs/lsp-zero.lua
@@ -16,69 +16,16 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = { 'cssls', 'graphql', 'html', 'lua_ls', 'tsserver' }, -- Exclude anything that needs the tool manually installed
   handlers = {
-    ruby_ls = lsp_zero.noop,
+    rust_analyzer = lsp_zero.noop,
+    gopls = lsp_zero.noop,
+    ruby_lsp = lsp_zero.noop,
   }
 })
 
 
 ---- Default Configurations
 
-lsp_zero.setup_servers({ 'cssls', 'graphql', 'html', 'gopls', 'html', 'lua_ls', 'rust_analyzer', 'tsserver' })
+lsp_zero.setup_servers({ 'cssls', 'graphql', 'html', 'gopls', 'html', 'lua_ls', 'ruby_lsp', 'rust_analyzer', 'tsserver' })
 
 
 ---- Custom Configurations
-
--- Ruby
--- textDocument/diagnostic support until 0.10.0 is released
-_timers = {}
-local function setup_diagnostics(client, buffer)
-  if require("vim.lsp.diagnostic")._enable then
-    return
-  end
-
-  local diagnostic_handler = function()
-    local params = vim.lsp.util.make_text_document_params(buffer)
-    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
-      if err then
-        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
-        vim.lsp.log.error(err_msg)
-      end
-      if not result then
-        return
-      end
-      vim.lsp.diagnostic.on_publish_diagnostics(
-        nil,
-        vim.tbl_extend("keep", params, { diagnostics = result.items }),
-        { client_id = client.id }
-      )
-    end)
-  end
-
-  diagnostic_handler() -- to request diagnostics on buffer when first attaching
-
-  vim.api.nvim_buf_attach(buffer, false, {
-    on_lines = function()
-      if _timers[buffer] then
-        vim.fn.timer_stop(_timers[buffer])
-      end
-      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
-    end,
-    on_detach = function()
-      if _timers[buffer] then
-        vim.fn.timer_stop(_timers[buffer])
-      end
-    end,
-  })
-
-  vim.api.nvim_create_autocmd("BufWritePre", {
-    callback = function()
-      vim.lsp.buf.format()
-    end,
-  })
-end
-
-require('lspconfig').ruby_ls.setup({
-  on_attach = function(client, buffer)
-    setup_diagnostics(client, buffer)
-  end,
-})
