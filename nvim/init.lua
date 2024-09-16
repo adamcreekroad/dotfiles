0178ca200a10d69e300a382c03b2c41cbe2a5662
Adam George <adam@adam-george.com>
2024-09-16 08:58:55 -0400

    Lua function to add Ruby frozen string comment
    This adds a function & mapping to quickly add the
    `frozen_string_literal: true` magic comment to the top of a ruby file,
    to avoid a couple of seconds any time I edit a file without it or
    create a new Ruby file.

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,3 +38,15 @@ else
 end
 
 vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
+
+
+-- Add ruby magic comment to the top of the file, since rubocop won't autocorrect it
+function rfs()
+  local pos = vim.api.nvim_win_get_cursor(0)
+  vim.api.nvim_win_set_cursor(0, { 1, 0 })
+  vim.api.nvim_buf_set_lines(0, 0, 0, false, { '# frozen_string_literal: true', '' })
+  pos[1] = pos[1] + 2
+  vim.api.nvim_win_set_cursor(0, pos)
+end
+
+vim.api.nvim_set_keymap('n', '<leader>rfs', '<cmd>lua rfs()<cr>', {})
